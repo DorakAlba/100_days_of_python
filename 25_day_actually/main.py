0728a8d016cfda509515625c93cc75080034ed1a
Yan Konyshev <48836120+DorakAlba@users.noreply.github.com>
2021-04-02 23:17:15 +0300

    Add files via upload

--- 25_day_actually/main.py
+++ 25_day_actually/main.py
@@ -18,8 +18,11 @@ print(data)
 answered = 0
 game_on = True
 answer = []
+all_state = data['state'].to_list()
 while game_on:
     take_answer = screen.textinput(title=f'{answered}/50 States Correct', prompt='State name')
+    if take_answer =="Exit":
+        break
     if take_answer in data.values and take_answer not in answer:
         answer.append(take_answer)
         answered += 1
@@ -29,4 +32,8 @@ while game_on:
         tu.write(take_answer, align='center', font=(('Arial'), 10, 'normal'))
     if answered ==50:
         game_on = False
-screen.exitonclick()
+
+for value in answer:
+    all_state.remove(value)
+all_s = pd.DataFrame(all_state)
+all_s.to_csv('you didnt know')
